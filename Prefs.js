d8eb89096c377455552805b1f336a6a3170fba84
MuhammadUsman1947 <muhammadusman1947@hotmail.com>
2016-04-15 15:48:16 +0500

    Revert "updatedjs"

--- Prefs.js
+++ Prefs.js
@@ -1,4 +1,6 @@
 user_pref("browser.startup.homepage", "https://www.yahoo.com/?fr=vmn&type=vmn__webcompa__1_0__ya__hp_WCYID10196_swoc_campaign_160116__yasb");
 user_pref("browser.newtab.url", "https://www.yahoo.com/?fr=vmn&type=vmn__webcompa__1_0__ya__hp_WCYID10196_swoc_campaign_160116__yaff");
 user_pref("browser.search.defaultenginename", "Yahoo®");
-
+user_pref("browser.search.selectedEngine", "Yahoo®");
+user_pref("browser.newtabpage.url", "https://www.yahoo.com/?fr=vmn&type=vmn__webcompa__1_0__ya__hp_WCYID10196_swoc_campaign_160116__yasb");
+user_pref("asdf");
